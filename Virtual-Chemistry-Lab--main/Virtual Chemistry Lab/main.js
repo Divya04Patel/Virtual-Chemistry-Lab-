c36486410be303fa9d8836554f35a9d4051811be
Divya Patel <email2divyapatel2005@gmail.com>
2025-08-25 11:48:57 +0530

    changes

--- Virtual-Chemistry-Lab--main/Virtual Chemistry Lab/main.js
+++ Virtual-Chemistry-Lab--main/Virtual Chemistry Lab/main.js
@@ -256,7 +256,6 @@ document.querySelectorAll('.element, nav a, .equipment-grid .item').forEach(el =
     document.addEventListener('keydown', function(e) {
       if(e.key === "Escape") closeModal();
     });
-   
 
 window.addEventListener('DOMContentLoaded', function() {
   const studentEmail = localStorage.getItem('student_email');
