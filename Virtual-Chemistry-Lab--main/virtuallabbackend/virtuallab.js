ca4aef1f5fbeee292690bb2b9cb86a0147d6b33c
Divya Patel <email2divyapatel2005@gmail.com>
2025-08-26 14:34:21 +0530

    changes

--- Virtual-Chemistry-Lab--main/virtuallabbackend/virtuallab.js
+++ Virtual-Chemistry-Lab--main/virtuallabbackend/virtuallab.js
@@ -235,7 +235,6 @@ fetch('http://127.0.0.1:5000/simulate', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ reactants: reactants })
-  })
     .then(response => response.json())
     .then(data => {
       console.log('Simulation result:', data);
