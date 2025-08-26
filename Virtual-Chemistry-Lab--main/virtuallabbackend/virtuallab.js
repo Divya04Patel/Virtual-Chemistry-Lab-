9672024d92830789e4568b546fe6aa79662f4363
Divya Patel <email2divyapatel2005@gmail.com>
2025-08-26 14:34:54 +0530

    changes

--- Virtual-Chemistry-Lab--main/virtuallabbackend/virtuallab.js
+++ Virtual-Chemistry-Lab--main/virtuallabbackend/virtuallab.js
@@ -238,6 +238,7 @@ fetch('http://127.0.0.1:5000/simulate', {
 })
     .then(response => response.json())
     .then(data => {
+    console.log('Simulation result:', data);
       // You can update your UI with the result here
     });
 }
