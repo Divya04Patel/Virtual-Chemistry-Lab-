eca38af41856898d2977931ff9a34fddfb66aa94
Divya Patel <email2divyapatel2005@gmail.com>
2025-09-09 00:34:31 +0530

    changes

--- Virtual-Chemistry-Lab--main/virtuallabbackend/virtuallab.js
+++ Virtual-Chemistry-Lab--main/virtuallabbackend/virtuallab.js
@@ -106,6 +106,7 @@ window.addEventListener('DOMContentLoaded', function() {
 });
 
 
+
 // About modal
 window.addEventListener('DOMContentLoaded', function() {
     document.getElementById('about-link').addEventListener('click', function(e) {
